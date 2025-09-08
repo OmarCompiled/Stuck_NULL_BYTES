0d2f762005998fb3b25c3106d9ccd7ef3281a5fd
Lethargy2718 <176531725+Lethargy2718@users.noreply.github.com>
2025-09-09 00:13:26 +0300

    fix: prevent physics error on changing scenes
    - Await a physics frame to make sure calculations are completed before the scene is freed

--- godot/game_manager.gd
+++ godot/game_manager.gd
@@ -15,6 +15,11 @@ func end_game():
 	# Manually reset singleton data since it doesn't reload on changing scenes
 	reset()
 	
-	# NOTE: get_tree().reload_current_scene() threw an error that would take some refactoring to fix. This is way easier to handle.
-	get_tree().change_scene_to_file("res://world.tscn") 
+	# NOTE: get_tree().reload_current_scene() threw an error that would take
+	# some refactoring to fix. change_scene_to_file is way easier to handle
+	
+	# Waiting for the physics frame to end prevents an error thrown on dying
+	# when freeing the scene before physics calculations are complete
+	await get_tree().physics_frame
+	get_tree().change_scene_to_file("res://world.tscn")
 	

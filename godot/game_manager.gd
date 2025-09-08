d7308a1a0da513796a6311c4512090efc84463cc
Lethargy2718 <176531725+Lethargy2718@users.noreply.github.com>
2025-09-08 20:13:10 +0300

    refactor: move quitting logic to singleton

--- godot/game_manager.gd
+++ godot/game_manager.gd
@@ -3,12 +3,18 @@ extends Node
 var enemies_killed: int = 0
 var total_enemies: int = 0
 
+func _process(_delta: float) -> void:
+	if Input.is_action_pressed("Quit"): # Esc 
+		get_tree().quit()
+
 func reset():
 	enemies_killed = 0
 	total_enemies = 0
 
 func end_game():
-	await get_tree().physics_frame
+	# Manually reset singleton data since it doesn't reload on changing scenes
 	reset()
-	get_tree().change_scene_to_file("res://world.tscn")
+	
+	# NOTE: get_tree().reload_current_scene() threw an error that would take some refactoring to fix. This is way easier to handle.
+	get_tree().change_scene_to_file("res://world.tscn") 
 	

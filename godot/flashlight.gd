c65433eb303f2168ef30e006647ba45aa8520b5a
Lethargy2718 <176531725+Lethargy2718@users.noreply.github.com>
2025-09-10 18:40:38 +0300

    feat: add accurate conic flashlight detection - Replace linear raycast with conic casting

--- godot/flashlight.gd
+++ godot/flashlight.gd
@@ -1,32 +1,71 @@
 extends Node3D
 
-@onready var ray = $RayCast3D
-@onready var spot_light = $SpotLight3D
+@onready var spot_light = $DetectionArea/SpotLight3D
 @onready var cooldown = $CoolDownBar
+@onready var detection_area = $DetectionArea
+@export var damage_per_second: float = 200
+@export var drain_per_second: float = 50
+@export var recharge_per_second: float = 100/3
 
-@export var dmg: float = 5
-@export var drain: float = 0.25
-@export var recharge: float = 0.15
+var is_flashlight_on = false
+var enemies_in_area = []
 
 func _input(_event: InputEvent) -> void:
-	if Input.is_action_pressed("Flash") and cooldown.value > 0:
-		spot_light.light_energy = 16
-	else:
-		spot_light.light_energy = 0
+	if Input.is_action_just_pressed("Flash") and cooldown.value > 0 and not is_flashlight_on:
+		_turn_flashlight_on()
+	elif Input.is_action_just_released("Flash") and is_flashlight_on:
+		_turn_flashlight_off()
 
-func _process(_delta: float) -> void:
-	if Input.is_action_pressed("Flash"):
-		cooldown.value = max(cooldown.value - drain, 0)
+func _process(delta: float) -> void:
+	if is_flashlight_on:
+		cooldown.value = max(cooldown.value - drain_per_second * delta, 0)
+		_damage_enemies_in_light(delta)
 	else:
-		cooldown.value += recharge
+		cooldown.value += recharge_per_second * delta
 		
-func _physics_process(_delta: float) -> void:
-	_attack()
+	if cooldown.value == 0:
+		_turn_flashlight_off()
+		
+func _turn_flashlight_on():
+	is_flashlight_on = true
+	spot_light.light_energy = 16
+	detection_area.body_entered.connect(_on_body_entered)
+	detection_area.body_exited.connect(_on_body_exited)
+	
+	# Add any enemies already in the area
+	for body in detection_area.get_overlapping_bodies():
+		if body is Shadow:
+			enemies_in_area.append(body)
+	
+func _turn_flashlight_off():
+	is_flashlight_on = false
+	spot_light.light_energy = 0
+	detection_area.body_entered.disconnect(_on_body_entered)
+	detection_area.body_exited.disconnect(_on_body_exited)
+	enemies_in_area.clear()
+	
+func _on_body_entered(body: Node3D):
+	if body is Shadow and not enemies_in_area.has(body):
+		enemies_in_area.append(body)
 
-func _attack():
-	if spot_light.light_energy > 0: # Only attack if the flashlight is on
-		ray.force_raycast_update()
-		if ray.is_colliding():
-			var collided_object = ray.get_collider()
-			if collided_object is Shadow:
-				collided_object.health_component.take_damage(dmg)
+func _on_body_exited(body: Node3D):
+	if body is Shadow and enemies_in_area.has(body):
+		enemies_in_area.erase(body)
+
+func _damage_enemies_in_light(delta: float):
+	for enemy in enemies_in_area:
+		if is_instance_valid(enemy) and _check_ray(enemy):
+			enemy.health_component.take_damage(damage_per_second * delta)
+
+func _check_ray(body: Node3D):
+	var space_state = get_world_3d().direct_space_state
+	var query = PhysicsRayQueryParameters3D.new()
+	
+	query.from = spot_light.global_transform.origin
+	query.to = body.global_transform.origin
+	query.exclude = [self]
+	
+	var result = space_state.intersect_ray(query)
+	
+	var in_sight = result and result.collider == body
+	return in_sight

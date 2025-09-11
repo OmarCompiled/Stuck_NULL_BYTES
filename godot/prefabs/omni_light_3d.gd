10319bde1c72fb788c2a11ffefea4e3328b486c0
Lethargy2718 <176531725+Lethargy2718@users.noreply.github.com>
2025-09-11 23:37:57 +0300

    refactor(heal): replace manual dimming with tweens

--- godot/prefabs/omni_light_3d.gd
+++ godot/prefabs/omni_light_3d.gd
@@ -2,19 +2,11 @@ extends OmniLight3D
 
 # The time it takes the room's light to turn off
 @export var dimming_seconds = 1
-var max_energy = light_energy
-var dimming_rate = max_energy / dimming_seconds
-var is_dimming = false
-
-func _process(delta: float) -> void:
-	if is_dimming:
-		_dim_light(delta)
-
-func _dim_light(delta: float) -> void:
-	light_energy = max(0, light_energy - dimming_rate * delta);
-	if light_energy == 0:
-		queue_free()
-
+var tween: Tween
 
 func _on_healing_area_room_health_depleted() -> void:
-	is_dimming = true
+	tween = create_tween()
+	tween.tween_property(self, "light_energy", 0.0, dimming_seconds)
+	tween.set_trans(Tween.TRANS_ELASTIC)
+	tween.set_ease(Tween.EASE_OUT)
+	tween.finished.connect(queue_free)

b40d482c942bc1af0f5bc1e1ed4b29393421cf66
Lethargy2718 <176531725+Lethargy2718@users.noreply.github.com>
2025-09-14 19:56:48 +0300

    fix: improve shard movement

--- godot/scripts/memory_fragment.gd
+++ godot/scripts/memory_fragment.gd
@@ -1,9 +1,9 @@
 extends CharacterBody3D
 class_name Collectible
 
-@export var attraction_force: float = 1.0
-@export var max_speed: float = 25.0
-@export var collect_distance: float = 0.7
+@export var acceleration: float = 25.0
+@export var max_speed: float = 200.0
+@export var collect_distance: float = 1.0
 @export var idle_rotation_speed: float = 1.0  # radians per second
 
 @onready var light = $FragmentLight
@@ -16,37 +16,55 @@ var energy_pulse = 6
 var range_pulse = 0.1
 var t = 0.0
 var freq = 2.0
+var current_speed: float = 0.0:
+	set(value):
+		current_speed = min(value, max_speed)
 
 
 func _on_detection_area_body_entered(body: Node3D) -> void:
 	if body is Player:
-		player = body
-		# Turn off collisions (phase through everything)
-		collision_layer = 0
-		collision_mask = 0
-		chasing = true
-
-
+		_start_chasing(body);
+		
+		
 func _physics_process(delta: float) -> void:
 	_update_light(delta)
 	
 	if chasing and player:
-		var to_player = player.global_position - global_position
-		var dist = to_player.length()
-
-		if dist < collect_distance:
-			_get_collected()
-			return
-			
-		var dir = to_player.normalized()
-		velocity = velocity.lerp(dir * max_speed, attraction_force * delta)
+		_chase(delta)
 	else:
-		rotate_y(idle_rotation_speed * delta)
-		velocity += get_gravity() * 2.5 * delta
+		_idle(delta)
 	
 	move_and_slide()
 	
-	
+func _start_chasing(body: Player):
+		chasing = true
+		player = body
+		
+		# Turn off collisions (phase through everything)
+		collision_layer = 0
+		collision_mask = 0
+		
+		velocity = Vector3.ZERO
+		
+		
+func _chase(delta: float) -> void:
+		var to_player = player.global_position - global_position
+		var dist = to_player.length()
+		
+		if dist < collect_distance:
+			_get_collected()
+			return
+		
+		current_speed += acceleration * delta
+		var dir = to_player.normalized()
+		velocity = dir * current_speed
+		
+		
+func _idle(delta: float) -> void:
+	rotate_y(idle_rotation_speed * delta)
+	velocity += get_gravity() * 2.5 * delta
+		
+		
 func _get_collected() -> void:
 	GameManager.currency += 1
 	queue_free()

35b62ff55aa9f38415b626ce09538be1e9985847
Lethargy2718 <176531725+Lethargy2718@users.noreply.github.com>
2025-09-15 22:27:01 +0300

    feat: make shadows play a sound on getting close

--- godot/scripts/shadow.gd
+++ godot/scripts/shadow.gd
@@ -2,14 +2,16 @@ extends CharacterBody3D
 class_name Shadow
 
 @export var health_component: HealthComponent
-@export var whoosh_sound_component: SoundComponent
+@export var chase_sound_component: SoundComponent
+@export var close_sound_component: SoundComponent
+@export var death_sound_component: SoundComponent
 @export var hit_sound_component: SoundComponent
 
 @export var damage_particles: PackedScene
 @export var death_light_scene: PackedScene
 @export var memory_fragment: PackedScene
 
-@export var sanity_reward = 8 # 5 was too low
+@export var sanity_reward = 8
 
 @export var knockback_decay: float = 0.95
 
@@ -25,9 +27,6 @@ class_name Shadow
 @export var max_whoosh_interval: float = 5.0
 @export_range(0.0, 1.0) var whoosh_probability: float = 1
 
-@export var min_death_pitch: float = 1.4
-@export var max_death_pitch: float = 1.6
-
 @onready var chase_timer = $ChaseTimer
 
 var speed: float = randf_range(min_speed, max_speed)
@@ -38,11 +37,13 @@ var knockback_duration = 0.7
 var shard_count: int = randi_range(min_shards, max_shards)
 var player: Player
 var look_for_player = false
-var has_played_first_whoosh = false  # Renamed for clarity
+var has_played_first_whoosh = false
+var can_play_close_sound = true
+var is_close = false;
 
 func _ready() -> void:
 	GameManager.total_enemies += 1
-	_reset_whoosh_timer()  # Start the timer initially
+	_reset_whoosh_timer()
 
 func _physics_process(delta: float) -> void:	
 	if is_knocked_back:
@@ -91,7 +92,9 @@ func _die():
 		_spawn_shard()
 		
 	_play_death_woosh()
-	whoosh_sound_component.kill()
+	chase_sound_component.kill()
+	close_sound_component.kill()
+	death_sound_component.kill()
 	hit_sound_component.kill()
 	
 	queue_free()
@@ -137,6 +140,7 @@ func _on_area_3d_body_exited(body: Node3D) -> void:
 	if body is not Player: return
 	look_for_player = false
 	can_see_player = false
+	has_played_first_whoosh = false
 
 func check_line_of_sight(target):
 	var space_state = get_world_3d().direct_space_state
@@ -160,16 +164,11 @@ func _on_chase_timer_timeout() -> void:
 	
 	
 func _play_random_whoosh():		
-	whoosh_sound_component.play()
+	chase_sound_component.play()
 		
 		
 func _play_death_woosh():
-	whoosh_sound_component.play(
-		min_death_pitch,
-		max_death_pitch,
-		whoosh_sound_component.min_volume + 25,
-		whoosh_sound_component.max_volume + 25
-	)
+	death_sound_component.play()
 
 
 func _reset_whoosh_timer():
@@ -182,3 +181,16 @@ func _reset_whoosh_timer():
 func hit():
 	hit_sound_component.play()
 	_reset_whoosh_timer()
+	
+	
+func _play_close_sound() -> void:
+	close_sound_component.play()
+	
+func _on_close_detection_area_body_entered(body: Node3D) -> void:
+	check_line_of_sight(body)
+	if body is Player and can_play_close_sound and can_see_player:
+		_reset_whoosh_timer()
+		_play_close_sound()
+		can_play_close_sound = false
+		await get_tree().create_timer(randf_range(5, 10)).timeout
+		can_play_close_sound = true

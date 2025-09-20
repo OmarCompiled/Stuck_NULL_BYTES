d1102c2f8fd95ee7dbc88c8240d9d0e16c5b10bd
OmarCompiled <179285620+OmarCompiled@users.noreply.github.com>
2025-09-20 12:02:20 +0300

    Add new font

--- godot/scripts/upgrades_menu.gd
+++
@@ -1,8 +0,0 @@
-extends Control
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta: float) -> void:
-	$MarginContainer/VBoxContainer/BarsContainer/RichTextLabel.text = "Memory Fragments: " + var_to_str(GameManager.currency)
-
-func _on_back_to_menu_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/menu/main_menu.tscn")

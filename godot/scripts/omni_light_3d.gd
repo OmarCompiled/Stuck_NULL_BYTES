d12edbdc9b685c104489c57861fb2466b6576e45
Lethargy2718 <176531725+Lethargy2718@users.noreply.github.com>
2025-09-20 22:44:54 +0300

    feat: dim healing room range instead of energy - Now it can still act as a landmark when depleted

--- godot/scripts/omni_light_3d.gd
+++ godot/scripts/omni_light_3d.gd
@@ -6,7 +6,6 @@ var tween: Tween
 
 func _on_healing_area_room_health_depleted() -> void:
 	tween = create_tween()
-	tween.tween_property(self, "light_energy", 0.0, dimming_seconds)
+	tween.tween_property(self, "omni_range", 1.5, dimming_seconds)
 	tween.set_trans(Tween.TRANS_ELASTIC)
 	tween.set_ease(Tween.EASE_OUT)
-	tween.finished.connect(queue_free)
